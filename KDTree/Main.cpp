88a252d7ee655f6b4ce75a6fe508ad37c3584fa6
nokotan <kamenokonokotan@gmail.com>
2021-06-11 18:33:33 +0900

    Add KDTree

---
+++ KDTree/Main.cpp
@@ -0,0 +1,81 @@
+# include <Siv3D.hpp>
+# include <emscripten.h>
+
+void RunMainLoop(void* arg)
+{
+	static_cast<const std::function<void()>*>(arg)->operator()();
+}
+
+void SetMainLoop(std::function<void()> mainLoop)
+{
+	emscripten_set_main_loop_arg(RunMainLoop, reinterpret_cast<void*>(&mainLoop), 0, 1);
+}
+
+struct Unit
+{
+	Circle circle;
+
+	ColorF color;
+
+	void draw() const
+	{
+		circle.draw(color);
+	}
+};
+
+// Unit を KDTree で扱えるようにするためのアダプタ
+struct UnitAdapter : KDTreeAdapter<Array<Unit>, Vec2>
+{
+	static const element_type* GetPointer(const point_type& point)
+	{
+		return point.getPointer();
+	}
+
+	static element_type GetElement(const dataset_type& dataset, size_t index, size_t dim)
+	{
+		return dataset[index].circle.center.elem(dim);
+	}
+};
+
+void Main()
+{
+	// 200 個の Unit を生成
+	Array<Unit> units;
+	{
+		for (size_t i = 0; i < 200; ++i)
+		{
+			Unit unit
+			{
+				.circle = Circle{ RandomVec2(Scene::Rect()), 4 },
+				.color = RandomColorF()
+			};
+			units << unit;
+		}
+	}
+
+	// kd-tree を構築
+	KDTree<UnitAdapter> kdTree{ units };
+
+	// radius search する際の探索距離
+	constexpr double searchDistance = 80.0;
+
+	SetMainLoop([&]{
+		System::Update();
+
+		const Vec2 cursorPos = Cursor::PosF();
+
+		Circle{ cursorPos, searchDistance }.draw(ColorF{ 1.0, 0.2 });
+
+		// searchDistance 以内の距離にある Unit のインデックスを取得
+		for (auto index : kdTree.radiusSearch(cursorPos, searchDistance))
+		{
+			Line{ cursorPos, units[index].circle.center }.draw(4);
+		}
+
+		// ユニットを描画
+		for (const auto& unit : units)
+		{
+			unit.draw();
+		}
+	});
+}
