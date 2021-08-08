cd3872e499e7bc04a086220f1894df4ad972a284
nokotan <kamenokonokotan@gmail.com>
2021-08-09 00:55:33 +0900

    Update Siv3D

--- Siv3D/lib/Siv3D.js
+++ Siv3D/lib/Siv3D.js
@@ -19,7 +19,7 @@ mergeInto(LibraryManager.library, {
         if (!window.keysBuffer) {
             window.keysBuffer = Module._malloc(349 /* GLFW_KEY_LAST + 1 */)
         }
-        Module.HEAPU8.set(window.keys, window.keysBuffer);
+        Module["HEAPU8"].set(window.keys, window.keysBuffer);
         return window.keysBuffer;
     },
     glfwGetKeysSiv3D__sig: "ii",
