2653b01090063044d189244aaad7ca8fff79c86b
nokotan <kamenokonokotan@gmail.com>
2021-09-12 15:02:59 +0900

    fix case sensitive

---
+++ QRReader/Main.cpp
@@ -0,0 +1,54 @@
+# include <Siv3D.hpp>
+
+void Main()
+{
+    Window::Resize(1280, 720);
+
+	Webcam webcam{ 0, Size{ 1280, 720 }, StartImmediately::Yes };
+
+	Image image;
+	DynamicTexture texture { 1280, 720 };;
+	QRScanner qrScanner;
+	Array<QRContent> contents;
+
+    Platform::Web::System::SetMainLoop([&]()
+    {
+        System::Update();
+        
+        if (!webcam)
+		{
+			if (SimpleGUI::Button(U"Retry", Vec2{ 20, 20 }))
+			{
+				webcam = Webcam{ 0, Size{ 1280, 720 }, StartImmediately::Yes };
+			}
+		}
+		
+		if (webcam.hasNewFrame())
+		{
+			webcam.getFrame(image);
+
+			texture.fill(image);
+
+			contents = qrScanner.scan(image);
+		}
+
+		// Webcam 作成待機中は円を表示
+		if (!webcam)
+		{
+			Circle{ Scene::Center(), 40 }
+				.drawArc(Scene::Time() * 180_deg, 300_deg, 5, 5);
+		}
+
+		if (texture)
+		{
+			texture.draw();
+		}
+
+		for (const auto& content : contents)
+		{
+			content.quad.drawFrame(4, Palette::Red);
+
+			PutText(content.text, Arg::topLeft = content.quad.p0);
+		}
+    });
+}
