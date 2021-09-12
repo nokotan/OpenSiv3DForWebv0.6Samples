02e4b48105b2e7a97f8caf8a4f0c83d800b78311
nokotan <kamenokonokotan@gmail.com>
2021-09-12 14:18:39 +0900

    Add samples

---
+++ AsyncFileDownload/Main.cpp
@@ -0,0 +1,31 @@
+# include <Siv3D.hpp>
+
+void Main()
+{
+    const URL url = U"https://raw.githubusercontent.com/Siv3D/siv3d.docs.images/master/logo/logo.png";
+	const FilePath saveFilePath = U"logo2.png";
+	AsyncHTTPTask task = SimpleHTTP::SaveAsync(url, saveFilePath);
+	Texture texture;
+
+    Platform::Web::System::SetMainLoop([&]()
+    {
+        System::Update();
+        
+        if (task.isReady())
+		{
+			if (task.getResponse().isOK())
+			{
+				texture = Texture{ saveFilePath };
+			}
+			else
+			{
+				Print << U"Failed.";
+			}
+		}
+
+		if (texture)
+		{
+			texture.draw();
+		}
+    });
+}
