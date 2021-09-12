8c02573d26cd0b03ce56b7181799202606a482bc
nokotan <kamenokonokotan@gmail.com>
2021-09-12 14:30:16 +0900

    Add samples

---
+++ RayIntersection/Main.cpp
@@ -0,0 +1,65 @@
+# include <Siv3D.hpp>
+
+template <class Shape>
+void Draw(const Shape& shape, const Ray& ray)
+{
+	if (auto pos = ray.intersectsAt(shape))
+	{
+		shape.draw(Linear::Palette::Orange);
+		Sphere{ *pos, 0.15 }.draw(Linear::Palette::Red);
+	}
+	else
+	{
+		shape.draw(HSV{ (shape.center.x * 60), 0.1, 0.95 }.removeSRGBCurve());
+	}
+}
+
+void Main()
+{
+    Window::Resize(1280, 720);
+	Graphics3D::SetGlobalAmbientColor(ColorF{ 0.25 });
+	const ColorF backgroundColor = ColorF{ 0.4, 0.6, 0.8 }.removeSRGBCurve();
+	const Texture uvChecker{ U"example/texture/uv.png", TextureDesc::MippedSRGB };
+	const MSRenderTexture renderTexture{ Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
+	DebugCamera3D camera{ renderTexture.size(), 30_deg, Vec3{ 10, 16, -32 } };
+
+	const Sphere sphere = Sphere{ { 2, 2, 8 }, 2 };
+	const Box box1 = Box::FromPoints({ -13, 0, 14 }, { -2, 4, 13 });
+	const Box box2 = Box::FromPoints({ -14, 0, 14 }, { -13, 2, 2 });
+	const Cylinder cy1{ {-8,2,4},{-4, 5,6}, 1 };
+	const Cylinder cy2{ {-10,2,1},{-6, 3,-2}, 0.5 };
+
+    Platform::Web::System::SetMainLoop([&]()
+    {
+        System::Update();
+        
+        camera.update(2.0);
+
+		// 3D
+		{
+			Graphics3D::SetCameraTransform(camera);
+			const Ray ray = camera.screenToRay(Cursor::PosF());
+			const ScopedRenderTarget3D target{ renderTexture.clear(backgroundColor) };
+			const OrientedBox ob1 = OrientedBox{ Arg::bottomCenter(-4, 0, 3), { 4, 2, 0.25 },  Quaternion::RotateY(Scene::Time() * -20_deg) };
+			const OrientedBox ob2 = OrientedBox{ Arg::bottomCenter(0, 0, -1), { 4, 2, 0.25 },  Quaternion::RotateY(Scene::Time() * 20_deg) };
+			const Cone cone{ { 4, 4, 0},{ 4, 7, 0}, 1.0, Quaternion::RotateZ(Scene::Time() * 10_deg) };
+
+			Plane{ 64 }.draw(uvChecker);
+			Draw(sphere, ray);
+			Draw(box1, ray);
+			Draw(box2, ray);
+			Draw(cy1, ray);
+			Draw(cy2, ray);
+			Draw(ob1, ray);
+			Draw(ob2, ray);
+			Draw(cone, ray);
+		}
+
+		// RenderTexture を 2D シーンに描画
+		{
+			Graphics3D::Flush();
+			renderTexture.resolve();
+			Shader::LinearToScreen(renderTexture);
+		}
+    });
+}
---
+++ ViewFrustum/Main.cpp
@@ -0,0 +1,71 @@
+# include <Siv3D.hpp>
+
+void DrawScene(const ViewFrustum& frustum, const Texture& groundTexture)
+{
+	Plane{ 60 }.draw(groundTexture);
+	const ColorF orange = ColorF{ Palette::Orange }.removeSRGBCurve();
+
+	for (auto z : Range(-5, 5))
+	{
+		for (auto x : Range(-8, 8))
+		{
+			for (auto y : Range(0, 8))
+			{
+				const Sphere s{ x,y,z, 0.2 };
+				s.draw(frustum.intersects(s) ? orange : ColorF{ 1.0 });
+			}
+		}
+	}
+}
+
+void Main()
+{
+    Window::Resize(1280, 720);
+	Graphics3D::SetGlobalAmbientColor(ColorF{ 0.5 });
+
+	const ColorF backgroundColor = ColorF{ 0.4, 0.6, 0.8 }.removeSRGBCurve();
+	const Texture groundTexture{ U"example/texture/uv.png", TextureDesc::MippedSRGB };
+	const MSRenderTexture renderTexture{ Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
+	const MSRenderTexture renderTexture2{ 360, 240, TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes };
+	DebugCamera3D camera1{ renderTexture.size(), 25_deg, Vec3{ 10, 16, -32 } };
+	BasicCamera3D camera2{ renderTexture2.size(), 25_deg };
+
+    Platform::Web::System::SetMainLoop([&]()
+    {
+        System::Update();
+        
+        camera1.update(4.0);
+		const Vec3 camera2Pos = Cylindrical{ 18, (Scene::Time() * 10_deg), (2 + Periodic::Sine0_1(4s) * 10) };
+		camera2.setView(camera2Pos, Vec3::Zero());
+		const Vec3 upDirection = Vec3{ Math::Sin(Scene::Time() * 0.2), 1, 0 }.normalized() * camera2.getLookAtOrientation();
+		camera2.setUpDirection(upDirection);
+		const ViewFrustum frustum{ camera2, 1.0, 50.0 };
+
+		// 俯瞰カメラ (camera1)
+		{
+			Graphics3D::SetCameraTransform(camera1);
+			const ScopedRenderTarget3D target{ renderTexture.clear(backgroundColor) };
+			DrawScene(frustum, groundTexture);
+			frustum.drawFrame(Palette::Red);
+			Line3D{ camera2Pos, camera2Pos + upDirection * 4 }.draw(Palette::Red);
+			OrientedBox{ frustum.getOrigin(), {1.2, 0.8, 2}, frustum.getOrientation() }.draw(ColorF{ 0.2 }.removeSRGBCurve());
+		}
+
+		// (camera2)
+		{
+			Graphics3D::SetCameraTransform(camera2);
+			const ScopedRenderTarget3D target{ renderTexture2.clear(backgroundColor) };
+			DrawScene(frustum, groundTexture);
+		}
+
+		// RenderTexture を 2D シーンに描画
+		{
+			Graphics3D::Flush();
+			renderTexture.resolve();
+			renderTexture2.resolve();
+			Shader::LinearToScreen(renderTexture);
+			RectF{ 360,240 }.drawShadow({ 0,0 }, 8, 3);
+			Shader::LinearToScreen(renderTexture2, Vec2{ 0,0 });
+		}
+    });
+}
